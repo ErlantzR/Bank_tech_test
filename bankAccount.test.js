a5fcbd5a199a0116918a53047d55d01c8818dbbe
Erlantz <erlantzram@gmail.com>
2022-06-20 12:30:46 +0100

    first test for withdraw method. It descreases balance by amount given

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -16,4 +16,15 @@ describe("BankAccount", () => {
       expect(account.balance).toBe(100);
     })
   })
+
+  describe("#withdrawMoney(amount)", () => {
+    it("decreases the balance by the amount given as an argument", () => {
+      const account = new BankAccount;
+      account.deposit(200);
+
+      account.withdraw(100);
+
+      expect(account.balance).toBe(100);
+    })
+  })
 })
