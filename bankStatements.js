4607410c05bb06496e640e1dd8ba38e7d9c30a13
Erlantz <erlantzram@gmail.com>
2022-06-21 14:55:49 +0100

    added and passed tests for createStatement method

--- bankStatements.js
+++ bankStatements.js
@@ -3,6 +3,30 @@ class BankStatements {
   constructor() {
     this.statements = ['date || credit || debit || balance'];
   }
+
+  createFormattedDate() {
+    const date = new Date();
+    const dateFormatted = date.toLocaleDateString();
+    return dateFormatted;
+  }
+
+  createStatement(reason, amount, balance) {
+    const dateFormatted = this.createFormattedDate();
+    const amount2D = amount.toFixed(2);
+    const balance2D = balance.toFixed(2);
+    switch (reason) {
+      case 'deposit':
+        const dStatement = `${dateFormatted} || ${amount2D} || || ${balance2D}`;
+        this.statements.push(dStatement);
+        break;
+      case 'withdraw':
+        const wStatement = `${dateFormatted} || || ${amount2D} || ${balance2D}`;
+        this.statements.push(wStatement);
+        break;
+      default:
+        throw new Error('Reason for statement not recognised');
+    }
+  }
 }
 
 module.exports = BankStatements;
--- bankStatements.test.js
+++ bankStatements.test.js
@@ -9,4 +9,24 @@ describe('BankStatements', () => {
     expect(bankStatements.statements)
         .toEqual(['date || credit || debit || balance']);
   });
+
+  describe('#createStatement(reason, amount)', () => {
+    it('"deposit", adds a credit transaction with amount and balance', () => {
+      bankStatements.createStatement('deposit', 1000, 2000);
+      const date = new Date();
+      const dateFormatted = date.toLocaleDateString();
+
+      expect(bankStatements.statements[1])
+          .toEqual(`${dateFormatted} || 1000.00 || || 2000.00`);
+    });
+
+    it('"withdraw", adds a debit transaction with amount and balance', () => {
+      bankStatements.createStatement('withdraw', 500, 1500);
+      const date = new Date();
+      const dateFormatted = date.toLocaleDateString();
+
+      expect(bankStatements.statements[1])
+          .toEqual(`${dateFormatted} || || 500.00 || 1500.00`);
+    });
+  });
 });
